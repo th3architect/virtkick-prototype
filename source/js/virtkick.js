7920e50b5ab38521d7b34589f4fe6d21a6a32976
Nowaker <damian.nowak@geozone.pl>
2014-08-06 05:17:42 +0200

    prototype alert on top

--- source/js/virtkick.js
+++ source/js/virtkick.js
@@ -2,3 +2,14 @@
 //= require bootstrap
 //= require vendor/bootstrap-combobox.js
 //= require vendor/bootstrap-sortable.js
+
+$(function() {
+  var token = 'remember-dismiss';
+  if(document.cookie.indexOf(token) == -1) {
+    $('.prototype-alert').removeClass('hidden');
+  }
+
+  $('.dismiss').click(function() {
+    document.cookie = token;
+  });
+});
